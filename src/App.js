e989cff3dfe18d1e84ae5b8c5977b84ab33c45f9
emsiri <emsirikami@gmail.com>
2022-01-15 12:27:36 +1000

    Got the image request working, now I need to hook it up to react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import {getEquppedImages} from './request'
+
+const characterId = '2305843009300358704'
 
 function App() {
   return (
--- src/request.js
+++ src/request.js
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 import dotenv from 'dotenv';
-import { openDB, closeDB, queryManifest } from "./manifest.js";
+import { openDb, closeDB, queryManifest } from "./manifest.js";
 dotenv.config();
 const membershipType = `3`
 const displayName = 'Emsiri%230594'
@@ -40,39 +40,34 @@ async function getEquipmentFromChar (characterId) {
   equipment = {
     items: equipment.data.items
   }
-  // console.log(Object.values(equipment.items))
   const equipmentedItemsHashes = equipment.items.map(el => el.itemHash >> 32)
   return equipmentedItemsHashes;
 }
 
-// returnData(apiKey, url)
-(async function() {
-  // const equippedItems = await getEquipmentFromChar(characterId).then((hash) => {
-    // console.log(hash);
-    // hash.forEach(el => {
-    //   queryManifest('DestinyInventoryItemDefinition', el)
-    // });
-    //(el => queryManifest('DestinyInventoryItemDefinition', el))
-    // queryManifest('DestinyInventoryItemDefinition', hash)
-  // })
-  const db = openDB();
+// (async function() {
+//   const db = await openDb();
+//   const equippedItemsHashes = await getEquipmentFromChar(characterId)
+//   const resultArray = await Promise.all(equippedItemsHashes.map(async (hash) => queryManifest(db, 'DestinyInventoryItemDefinition', hash)));
+//   const parsedResult = resultArray.map(el => {
+//     const parsedData = JSON.parse(el.json);
+//     const imageUrl = parsedData.displayProperties.icon
+//     return imageUrl;
+//   })
+//   const fullImageUrls = parsedResult.map(el => `https://www.bungie.net/${el}`)
+//   console.log(fullImageUrls)
+//   closeDB(db);
+// })()
+
+export async function getEquppedImages(characterId) {
+  const db = await openDb();
   const equippedItemsHashes = await getEquipmentFromChar(characterId)
-  const equippedItemsHashesFirst = equippedItemsHashes[0]
-  console.log(equippedItemsHashesFirst)
-  const equippedItems = queryManifest(db, 'DestinyInventoryItemDefinition', equippedItemsHashes[0]);
-  // console.log(equippedItemsHashes)
-  console.log(equippedItems)
+  const resultArray = await Promise.all(equippedItemsHashes.map(async (hash) => queryManifest(db, 'DestinyInventoryItemDefinition', hash)));
+  const parsedResult = resultArray.map(el => {
+    const parsedData = JSON.parse(el.json);
+    const imageUrl = parsedData.displayProperties.icon
+    return imageUrl;
+  })
+  const fullImageUrls = parsedResult.map(el => `https://www.bungie.net/${el}`)
+  console.log(fullImageUrls)
   closeDB(db);
-  // const equippedItemsImages = await equippedItems.flat().map(el => el)
-  // console.log(equippedItemsImages);
-})()
-
-// getEquipmentFromChar(characterId)
-// .then((hash) => hash.map(hash => queryManifest('DestinyInventoryItemDefinition', hash)))
-// .then(el => el.displayProperties)
-
-// .then(data => console.log(data))
-// .then(data => console.log(data.Response.profileInventory))
-// .then(data => console.log(data.Response.character.data))
-// .then(data => console.log(data.Response.profile.data.characterIds))
-// .catch(err => console.log(err))
+}
