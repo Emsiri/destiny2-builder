be1b332ad63ad16d12a205d47cb05cd87872ff16
emsiri <emsirikami@gmail.com>
2022-03-14 10:19:35 +1000

    Added healthpoint check test

--- server/app.test.js
+++ server/app.test.js
@@ -2,6 +2,11 @@ import request from "supertest";
 import app from "./app";
 const characterId = "2305843009300358704";
 
+test("It should check the health endpoint returns correctly", async () => {
+  const response = await request(app).get(`/v1/health`);
+  expect(response.statusCode).toBe(200);
+});
+
 test("It should get the equipped items", async () => {
   const response = await request(app).get(`/v1/equipped/${characterId}`);
   expect(response.statusCode).toBe(200);
